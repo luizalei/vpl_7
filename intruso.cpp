780023de6b3ce660be5a23ddad2ba1bc9efcafa0
Luiza <luizalei8@gmail.com>
2022-10-25 21:59:54 -0300

    vai

---
+++ intruso.cpp
@@ -0,0 +1,13 @@
+#include "intruso.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+
+    void set_senha_vazada(std::string vazou)
+    {
+        _senha=vazou;
+    }
+    std::string crack_senha()
+    {
+
+    }
