7bb8ecccd74c79807b431cf8a359b372a92ef9af
Victoriamachado21 <victoriam.ufmg@gmail.com>
2022-10-31 23:46:14 -0300

    atualização da intruso hpp

--- intruso.hpp
+++ intruso.hpp
@@ -1,24 +1,14 @@
-#ifndef intruso
-#define intruso
 #include <string>
-#include <iostream>
-#include <vector>
 
-class Intruso{
-    private:  
-        std::string _senha;
-        std::vector<int> _num;
-        std::vector<int> _A;
-        std::vector<int> _B;
-        std::vector<int> _C;
-        std::vector<int> _D;
-        std::vector<int> _E;
-        std::vector<int> _comp; 
+class Intruso
+{
+private:
+  std::string letters[10];
+  std::string numbers[10];
+  std::string senha = "";
+  int count = 0;
 
-    public:
-        void set_senha_vazada(std::string vazou);
-        std::string crack_senha();
-        Intruso(std::vector<int> num, std::string senha);
+public:
+  void set_senha_vazada(std::string vazou);
+  std::string crack_senha();
 };
-
-#endif 
