d5b00a0210afc1264f3f609d199bb0ce57854c4e
Luiza <luizalei8@gmail.com>
2022-10-31 21:47:28 -0300

    salvar

--- intruso.cpp
+++ intruso.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 
-    Intruso::Intruso (std::vector<int> num, std::string senha)
+    Intruso::Intruso (std::vector<std::vector<int>> num, std::string senha)
     {
         _num= num;
         _senha= senha;
@@ -11,34 +11,39 @@
 
     void Intruso:: set_senha_vazada(std::string vazou)
     {
-        for(size_t i=0;i<vazou.size();i++)
+        int j=0;  
+        for(int i=0;i<vazou.size();i++)
         {
             if(i<10)
             {
-                _num[i]=vazou[i];
+                _num[j][i]=vazou[i];
             }else{
-               _senha[i]=vazou[i];
+                _senha[i+(j*5)]=vazou[i];
             }
         }
-        _A.push_back(_num[0]); 
-        _A.push_back(_num[1]); 
-        _B.push_back(_num[2]); 
-        _B.push_back(_num[3]); 
-        _C.push_back(_num[4]); 
-        _C.push_back(_num[5]); 
-        _D.push_back(_num[6]); 
-        _D.push_back(_num[7]); 
-        _E.push_back(_num[8]); 
-        _E.push_back(_num[9]); 
+        _A.push_back(_num[j][0]); 
+        _A.push_back(_num[j][1]); 
+        _B.push_back(_num[j][2]); 
+        _B.push_back(_num[j][3]); 
+        _C.push_back(_num[j][4]); 
+        _C.push_back(_num[j][5]); 
+        _D.push_back(_num[j][6]); 
+        _D.push_back(_num[j][7]); 
+        _E.push_back(_num[j][8]); 
+        _E.push_back(_num[j][9]); 
+        j++;
     }
           
-    std::string crack_senha()
+    std::string Intruso::crack_senha()
     {
-       
+        int i=0,j=0;
+        for(i=0;i<_num.size();i++){
+            _senha[i]
+        }
 
 
-       
-    std:: string senha;
+
+    std::string senha;
     std::vector<int> n;
     for(size_t i = 0; i < m.size(); i++){
         if(i != 3*n){
